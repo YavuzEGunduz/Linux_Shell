a77351e12bb514de2853a909a4ec1998e4c64554
YavuzEG <yavuzemregunduz@gmail.com>
2024-12-27 21:47:05 +0300

    Last Changes

--- src/signal_handler.c
+++ src/signal_handler.c
@@ -1,3 +1,8 @@
+//B221210080 ÇAĞRI TÜKENMEZ
+//B221210038 DENİZ UMUR
+//B231210350 MELİH DEDEOĞLU
+//B221210071 YAVUZ EMRE GÜNDÜZ
+//B211210015 BUĞRA BASTABAN
 #include "signal_handler.h"
 #include <stdio.h>
 #include <stdlib.h>
